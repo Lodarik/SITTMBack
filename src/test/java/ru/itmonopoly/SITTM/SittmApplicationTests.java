4fae98d049c1a02096f8c82b47ba76b06830540e
DD <dmitry@kishkin.su>
2019-03-20 21:54:01 +0300

    REST,DB.initial MVC

---
+++ src/test/java/ru/itmonopoly/SITTM/SittmApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.itmonopoly.SITTM;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SittmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
