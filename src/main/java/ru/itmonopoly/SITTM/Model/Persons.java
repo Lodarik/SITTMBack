35cdcd5bc21c335caf5cdb1d2aa428ce6c6e87fc
DD <dmitry@kishkin.su>
2019-04-04 16:00:33 +0300

    Full Task Model, connection table OneToOne with Persons

--- src/main/java/ru/itmonopoly/SITTM/Model/Persons.java
+++ src/main/java/ru/itmonopoly/SITTM/Model/Persons.java
@@ -1,5 +1,35 @@
 package ru.itmonopoly.SITTM.Model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Persons")
 public class Persons {
+	@Id
+	@Column(name ="login")
+	private String login;
+	private String name;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
