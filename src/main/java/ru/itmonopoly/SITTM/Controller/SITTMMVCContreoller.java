72a9cdbb9bdfe22444a9705f745886283e462689
DD <dmitry@kishkin.su>
2019-04-09 01:13:52 +0300

    Thymeleaf, redirect user page, add attribute user

--- src/main/java/ru/itmonopoly/SITTM/Controller/SITTMMVCContreoller.java
+++ src/main/java/ru/itmonopoly/SITTM/Controller/SITTMMVCContreoller.java
@@ -3,13 +3,22 @@ package ru.itmonopoly.SITTM.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SITTMMVCContreoller {
-    @GetMapping({"/", "/index"})
+    @GetMapping(value={"/", "/index"})
     public String index(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
         model.addAttribute("name", name);
         return "index";
+        
     }
+    @PostMapping(value= {"/","/index"})
+    public String postindex(Model model, @RequestParam(value = "user") String user) {
+    	model.addAttribute("user",user);
+    	return "user";
+    	
+    }
+    
 }
